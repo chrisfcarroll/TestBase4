3d9543dd95e24d4902e99c4d21ea8911ad97a31d
Chris F Carroll <chris.carroll@unforgettable.me.uk>
2015-03-11 23:20:17 +0000

    AutoFixture First test passed

---
+++ TestBase.AutoFixture/TestBaseFor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestBase
+{
+    public class TestBaseFor<T>
+    {
+        protected internal T UnitUnderTest;
+
+        [SetUp]
+        protected virtual void CreateUnitUnderTest()
+        {
+            UnitUnderTest = Activator.CreateInstance<T>();
+        }
+
+    }
+}
