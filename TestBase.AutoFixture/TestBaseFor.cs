4784f603d851a808bc5b54652b5c848778f1ca49
Chris F Carroll <chris.carroll@unforgettable.me.uk>
2015-03-16 23:46:29 +0000

    Create UnitUnderTests having up to 1 constructor parameter

--- TestBase.AutoFixture/TestBaseFor.cs
+++ TestBase.AutoFixture/TestBaseFor.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Linq;
 using NUnit.Framework;
 
 namespace TestBase
 {
+    /// <summary>
+    /// A Base Class for TestFixtures / TestClasses which will auto-construct a UnitUnderTest
+    /// </summary>
+    /// <typeparam name="T">The <see cref="System.Type"/> of the <see cref="UnitUnderTest"/></typeparam>
     public class TestBaseFor<T>
     {
         protected internal T UnitUnderTest;
@@ -10,8 +15,27 @@ namespace TestBase
         [SetUp]
         protected virtual void CreateUnitUnderTest()
         {
-            UnitUnderTest = Activator.CreateInstance<T>();
+            UnitUnderTest = (T)CreateInstanceByMakingUpParameters(typeof(T));
         }
 
+        protected internal object CreateInstanceByMakingUpParameters(Type type)
+        {
+            var constructor = type.GetConstructors().FirstOrDefault();
+            if (type == typeof (string))
+            {
+                return typeof(string).Name;
+            }
+            else if (type.IsValueType || constructor==null || constructor.GetParameters().Length == 0)
+            {
+                return Activator.CreateInstance(type);
+            }
+            else
+            {
+                var pars = constructor.GetParameters()
+                            .Select(p => CreateInstanceByMakingUpParameters(p.ParameterType))
+                            .ToArray();
+                return Activator.CreateInstance(type, pars);
+            }
+        }
     }
 }
---
+++ TestBase4.Specifications/AutoFixture/WhenYou_RunAFixtureTestingAClassWithNoConstructorParameters.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using TestBase;
+
+namespace TestBase4.Specifications.AutoFixture
+{
+    [TestFixture]
+    class WhenYou_RunAFixtureTestingAClassWithNoConstructorParameters : TestBaseFor<WhenYou_RunAFixtureTestingAClassWithNoConstructorParameters.ClassWithDefaultConstructor>
+    {
+        [Test]
+        public void ThenI_SetUnitUnderTestToAnInstanceOfT()
+        {
+            Assert.IsNotNull(UnitUnderTest);
+            Assert.That(UnitUnderTest, Is.AssignableTo<ClassWithDefaultConstructor>());
+        }
+
+        // ReSharper disable once ClassNeverInstantiated.Local
+        internal class ClassWithDefaultConstructor { }
+    }
+}
